2ca208f7b92d522bf5367a731a86420a14a7b4f5
andrewminai24 <andrewminai24@gmail.com>
2019-07-09 18:32:36 -0700

    Chapter 2

---
+++ JavaProgram1/src/Error4.java
@@ -0,0 +1,8 @@
+class Practice2 {
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 2;
+        double average = (num1 + num2) / 2.0;
+        System.out.println(average);
+    }
+}
