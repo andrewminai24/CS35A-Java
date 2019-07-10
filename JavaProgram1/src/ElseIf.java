7d93c0a98bfeb331152e871eeb613ef95a76bf29
andrewminai24 <andrewminai24@gmail.com>
2019-07-10 16:36:25 -0700

    else if

---
+++ JavaProgram1/src/ElseIf.java
@@ -0,0 +1,14 @@
+class ElseIf{
+    public static void main(String[] args){
+        int age = 55;
+
+        if(age > 60)
+            System.out.println("You are a senior citizen");
+        else if(age <=35)
+        {
+            System.out.println("You are middle aged");
+        }else {
+            System.out.println("You are young");
+        }
+    }
+}
