7010d3b911780d1e1c7a27c03593e9b64e7919de
andrewminai24 <andrewminai24@gmail.com>
2019-07-09 18:24:27 -0700

    first javaprogram

---
+++ JavaProgram1/src/Error3.java
@@ -0,0 +1,8 @@
+public class Practice1 {
+
+    public static void main(String[] args) {
+        System.out.println(1.0 - 0.1 - 0.1 - 0.1 - 0.1 - 0.1);
+
+        System.out.println(1.0 - 0.9);
+    }
+}
---
+++ JavaProgram1/src/Main.java
@@ -0,0 +1,22 @@
+
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello Andrew");
+
+        var x = new Hello(); // This is creating a object.
+        x.tacos();
+    }
+    public void tacos(){
+        System.out.println("Hello there tacos!!");
+    }
+
+}
+//What static is saying that we do not need to a instance to run the program
+//Or meaning we do not need to create a object like in cpp.
+//class contains everything - has members
+//methods - function
+// properties- store something
+// access modifier(public) who can use
+// static no instance of class needed
+// object instance of class
