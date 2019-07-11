778dd2ef3b405c352fa211645b8da810440468fa
andrewminai24 <andrewminai24@gmail.com>
2019-07-10 18:11:42 -0700

    Array modified

--- JavaProgram1/src/Array.java
+++ JavaProgram1/src/Array.java
@@ -1,12 +1,6 @@
 class Array{
     public static void main(String[] args){
-        int arr[] = new int[10];
-
-
-        arr[0]= 23;
-        arr[1]= 2;
-        arr[2]= 3;
-        arr[3]= 4;
+        int arr[] = {1,23,2,34,4};
 
 
         System.out.println(arr[2]);
