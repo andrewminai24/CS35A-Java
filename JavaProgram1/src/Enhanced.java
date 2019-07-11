443ad00e80cae75fd10d00be318d8e574e822461
andrewminai24 <andrewminai24@gmail.com>
2019-07-10 18:42:32 -0700

    Array modified

---
+++ JavaProgram1/src/Enhanced.java
@@ -0,0 +1,15 @@
+class Enhanced{
+    public static void main(String[] args) {
+        int arr[] = {2, 3, 4, 5, 6, 7};
+        change(arr);
+
+        for(int y; arr)
+            System.out.println(y);
+    }
+
+    public static void Change(int x[]){
+        for(int i=0;i < x.length;i++)
+            x[i]+=5;
+    }
+
+}
