5e3a09915032311dba65b85ea71c49c975fca7ad
andrewminai24 <andrewminai24@gmail.com>
2019-07-10 17:08:41 -0700

    math methods

--- JavaProgram1/src/math.java
+++ JavaProgram1/src/math.java
@@ -1,5 +1,18 @@
 class Math{
     public static void main(String[] args){
-        System.out.println(Math.floor(7.8));
+        System.out.println(Math.abs(7.8));
     }
 }
+//This Math.abs gives you the absolute value of a number
+
+//Math.ceil this rounds the number up
+
+
+//Math.floor rounds the number down
+
+
+//This Math.max that can take multiple parameters
+
+//This Math.min takes the lesser of two numbers
+
+//This Math.sqrt takes the square root of a number
