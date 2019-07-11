32982719558dfab1b2c89b70381402a9e7aa011a
andrewminai24 <andrewminai24@gmail.com>
2019-07-10 18:09:51 -0700

    Arrays in Java

---
+++ JavaProgram1/src/Array.java
@@ -0,0 +1,15 @@
+class Array{
+    public static void main(String[] args){
+        int arr[] = new int[10];
+
+
+        arr[0]= 23;
+        arr[1]= 2;
+        arr[2]= 3;
+        arr[3]= 4;
+
+
+        System.out.println(arr[2]);
+
+    }
+}
